b0a8f24b4b1509ed2168266c55b98f6ba49b33ca
Daniel Roy Greenfeld <daniel@feldroy.com>
2025-10-05 12:56:45 +0800

    Better form handling
    Co-authored-by: Audrey Roy Greenfeld <audrey@feldroy.com>

--- main.py
+++ main.py
@@ -9,25 +9,35 @@ mxbai = AsyncMixedbread(api_key=getenv('MIXEDBREAD_APIKEY'))
 
 
 @app.page
-def index():
+def index(q: str | None = None):
     title = 'AirBread'
     return air.layouts.picocss(
         air.Title(title),
+        air.Script("""function updateQinURL() {
+            let url = new URL(window.location);
+            const value = document.getElementById('q').value
+            url.searchParams.set('q', value);
+            window.history.pushState({}, '', url);            
+        };
+        """),        
         air.H1(title),
         air.P('Mixedbread AI search of Air documentation and code.'),
         air.Form(
             air.Fieldset(
-                air.Input(type="search", name="q", placeholder="Search"),
-                air.Input(type="submit", value="Go!"),
+                air.Input(type="search", id="q", value=q, name="q", placeholder="Search", autofocus=True),
+                air.Button(
+                    "Search",
+                    hx_get="/search",
+                    hx_target="#results",
+                    hx_include="#q",
+                    onclick="updateQinURL()",
+                ),                
+                # air.Input(type="submit", value="Go!"),
                 role="group",
-            ),
-            hx_get="/search",
-            hx_target='#result',
-            hx_swap="none",             
+            ), 
         ),
-
         air.Div(
-            air.Article(air.Pre(air.Code("Response will go here")), id="result"),
+            air.Article(air.Pre(air.Code("Response will go here")), id="results"),
         ),        
     )
 
@@ -44,6 +54,6 @@ async def search(q: str):
             air.Raw(markdown(f'{chunk.text or 'Non-text content'}')),
             air.Hr()
         ) for chunk in res.data],
-        id='result',
+        id='results',
         hx_swap_oob="true"
     )
